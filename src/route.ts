2a100259a11199040c1d7938eb426c0563d2e727
vivek <vivekjalondhara011@gmail.coim>
2024-11-29 11:46:06 +0530

    solve bug

--- src/route.ts
+++ src/route.ts
@@ -5,8 +5,8 @@ import taskRoute from "./module/Task/routes";
 import authRoute from "./module/auth/routes";
 
 const configureRoutes = (app: Application) => {
-  app.use("/api/task", taskRoute());
+  app.use("/api/tasks", taskRoute());
   app.use("/api/auth", authRoute());
 };
 
-export default configureRoutes;
+export default configureRoutes
